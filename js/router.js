46cd68f85ecaf68c8526e69a44e30df1c182471d
Fernandabitten <fernandab.bittencourt@gmail.com>
2022-05-26 20:02:06 -0300

    up

---
+++ js/router.js
@@ -0,0 +1,32 @@
+export class Router {
+  routes = {}
+ add(routeName, page){
+  this.routes[routeName] = page
+ }
+ 
+  route(event){
+    event = event || window.event
+    event.preventDefault()
+    window.history.pushState({}, "", event.target.href)    
+   this.handle()
+  }
+  
+  handle() {
+    const {pathname} = window.location
+    let container = document.querySelector('#backgroundImg'); 
+    if(pathname === "/"){
+      container.style.backgroundImage = "url('../img/mountains-universe.png')";
+    } else if (pathname === "/universe") {
+     container.style.backgroundImage = "url('../img/universe.png')";
+    } else if (pathname === "/exploration"){
+      container.style.backgroundImage = "url('../img/exploration.png')";
+    }    
+    const route = this.routes[pathname] || this.routes[404]
+    fetch(route)
+    .then(data => data.text())
+    .then(html => {
+      document.querySelector('#app').innerHTML = html
+    })
+  }
+}
+
